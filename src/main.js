f237c2723822cb1f92b06da21ff1edd810bb8bf3
CoderMageFox <codermagefox@codermagefox.com>
2018-06-03 11:00:13 +0800

    a simply 0.0.1 updated

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import "../src/assets/CSS/reset.css"
+import "./assets/CSS/reset.css"
 import Vue from 'vue'
 import App from './App'
 import 'lib-flexible/flexible.js'
